0dd141e5be01e609b23a546f4f9be67037543d70
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-03-02 10:52:11 +0100

    feat: add GameSettings interface. add current settings object. add update functions for selection display.

--- src/ts/settings.ts
+++ src/ts/settings.ts
@@ -1,15 +1,20 @@
 import "../styles/main.scss";
 import "../styles/components/_buttons.scss";
 
-class Settings {
+interface GameSettings {
   theme: "code" | "foods";
-  player: "blue" | "orange";
-  boardSize: "16" | "24" | "36";
+  player: "blue" | "orange" | null;
+  boardSize: "16" | "24" | "36" | null;
+}
+
+class Settings {
+  currentSettings: GameSettings = {
+    theme: "code",
+    player: null,
+    boardSize: null,
+  };
 
   constructor() {
-    this.theme = "code";
-    this.player = "blue";
-    this.boardSize = "16";
     this.init();
   }
 
@@ -20,15 +25,15 @@ class Settings {
     this.initThemeOnHoverListeners();
 
     this.initOnChangeListener("theme", (value) => {
-      this.theme = value as "code" | "foods";
+      this.currentSettings.theme = value as "code" | "foods";
     });
 
     this.initOnChangeListener("player", (value) => {
-      this.player = value as "blue" | "orange";
+      this.currentSettings.player = value as "blue" | "orange";
     });
 
     this.initOnChangeListener("boardSize", (value) => {
-      this.boardSize = value as "16" | "24" | "36";
+      this.currentSettings.boardSize = value as "16" | "24" | "36";
     });
   }
 
@@ -45,7 +50,7 @@ class Settings {
         }
       });
       label.addEventListener("mouseout", () => {
-        this.updateThumbnail(this.theme);
+        this.updateThumbnail(this.currentSettings.theme);
       });
     });
   }
@@ -54,13 +59,15 @@ class Settings {
    * Initiates an event listener for input changes.
    *
    * @param name - The input group name
-   * @param onChange - Callback invoked with the new value when the input changes
+   * @param onChangeCallback - Callback invoked with the new value when the input changes
    */
-  private initOnChangeListener(name: string, onChange: (value: string) => void): void {
+  private initOnChangeListener(name: string, onChangeCallback: (value: string) => void): void {
     const inputs = document.querySelectorAll<HTMLInputElement>(`input[name='${name}']`);
     inputs.forEach((input) => {
       input.addEventListener("change", () => {
-        onChange(input.value);
+        onChangeCallback(input.value);
+        this.updateStartButton();
+        this.updateSelectionDisplay();
       });
     });
   }
@@ -75,6 +82,67 @@ class Settings {
     if (thumbnail) {
       thumbnail.src = `/memory-game/assets/img/thumbnails/${inputValue}_theme_thumbnail.png`;
     }
+  }
+
+  /**
+   * Enables the start button when player and board size are selected, otherwise disables it.
+   */
+  private updateStartButton(): void {
+    const startButton = document.querySelector<HTMLButtonElement>("#start-button");
+    if (startButton) {
+      if (this.areSettingsComplete()) {
+        startButton.disabled = false;
+      } else {
+        startButton.disabled = true;
+      }
+    }
+  }
+
+  /**
+   * Updates the displayed selections.
+   */
+  private updateSelectionDisplay(): void {
+    this.updateSpanText("#selection-theme", `${this.currentSettings.theme} theme`);
+
+    if (this.currentSettings.player) {
+      this.updateSpanText("#selection-player", `${this.currentSettings.player} Player`);
+    }
+
+    if (this.currentSettings.boardSize) {
+      this.updateSpanText("#selection-boardSize", `Board-${this.currentSettings.boardSize}-Cards`);
+    }
+
+    this.updateDividerDisplay();
+  }
+
+  /**
+   * Toggles the divider appearance based on whether all settings are complete.
+   */
+  private updateDividerDisplay(): void {
+    const container = document.querySelector(".settings__selection");
+    container?.classList.toggle("settings--complete", this.areSettingsComplete());
+  }
+
+  /**
+   * Updates the span text of the element.
+   *
+   * @param selector - CSS selector of the span element to update
+   * @param text - Text to be updated in the span
+   */
+  private updateSpanText(selector: string, text: string | null): void {
+    const element = document.querySelector<HTMLElement>(selector);
+    if (element && text) {
+      element.innerText = text;
+    }
+  }
+
+  /**
+   * Checks if the player and board size are set.
+   *
+   * @returns - True if player and board size are set, otherwise false
+   */
+  private areSettingsComplete(): boolean {
+    return this.currentSettings.player !== null && this.currentSettings.boardSize !== null;
   }
 }
 
