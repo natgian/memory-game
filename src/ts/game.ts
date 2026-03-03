85e8d7a287df1b302686e78f22d8e3dc202402d9
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-03-03 11:10:34 +0100

    feat: refactor get game settings method, add applyTheme method

--- src/ts/game.ts
+++ src/ts/game.ts
@@ -2,16 +2,37 @@ import "../styles/main.scss";
 import { GameSettings } from "./interfaces";
 
 class Game {
+  currentSettings: GameSettings = {
+    theme: "code",
+    player: null,
+    boardSize: null,
+  };
+
   constructor() {
-    this.getCurrentSettings();
+    this.getGameSettings();
+    this.applyTheme();
   }
 
-  private getCurrentSettings() {
-    const currentSettings = sessionStorage.getItem("gameSettings");
-    if (currentSettings) {
-      const gameSettings = JSON.parse(currentSettings) as GameSettings;
-      console.log(gameSettings);
+  /**
+   * Gets the game settings from the session storage and assigns them to currentSettings.
+   * If there are no game settings in the session storage it redirects the user to the settings page.
+   */
+  private getGameSettings(): void {
+    const gameSettings = sessionStorage.getItem("gameSettings");
+
+    if (gameSettings) {
+      this.currentSettings = JSON.parse(gameSettings) as GameSettings;
+    } else {
+      window.location.href = "settings.html";
     }
+  }
+
+  /**
+   * Applies the theme based on currentSettings.
+   */
+  private applyTheme(): void {
+    document.body.classList.add(`theme--${this.currentSettings.theme}`);
+    document.querySelector("#current-player")?.classList.add(`player__icon--${this.currentSettings.player}`);
   }
 }
 
