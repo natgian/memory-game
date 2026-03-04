99c01a00927729cd150248b18694e8aa224c064a
Nathalie Giancaspro <n.giancaspro@outlook.com>
2026-03-04 08:41:32 +0100

    feat: add Card interface

--- src/ts/interfaces.ts
+++ src/ts/interfaces.ts
@@ -1,5 +1,10 @@
 export interface GameSettings {
   theme: "code" | "foods";
-  player: "blue" | "orange" | null;
-  boardSize: "16" | "24" | "36" | null;
+  player: "blue" | "orange";
+  boardSize: "16" | "24" | "36";
+}
+
+export interface Card {
+  name: string;
+  img: string;
 }
